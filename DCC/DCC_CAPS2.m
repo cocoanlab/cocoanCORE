3eb7c291431d11224cbf95c3b6e1f8269f8a38bb
jaejoonglee92 <jaejoonglee92@gmail.com>
2019-03-28 18:40:37 +0900

    dcc

---
+++ DCC/DCC_CAPS2.m
@@ -0,0 +1,239 @@
+function DCC_mat = DCC_CAPS2(roi_values, varargin)
+% function DCC_mat = DCC_CAPS2(roi_values, varargin)
+%
+% Estimate a multivariate GARCH model using the DCC estimator of Engle and Sheppard
+% 
+% INPUTS:
+%
+%     roi_values      Timeseries of ROI values. T * P matrix (Time * Parcel)
+%
+%
+% Optional Inputs:
+%
+%     'whiten'
+%                     Use ARMA(1, 1) model to remove autocorrelation
+%
+%     'simple'
+%                     Estimate parameters from the whole connections.
+%                     Faster than connection-wise parameter estimation.
+%                     However, assumption may not be accurate.
+%
+%     'residualize'
+%                     Followed by model matrix X, [time points x predictors], to remove
+%                     prior to whitening and dynamic correlation estimation
+%
+%     'parellel'
+%                     For parellel computing.
+%
+%     'dosaveload' 
+%                     Only without 'simple' option.
+%                     Followed by savedir, to save and load data from savedir
+%                     which contains and will contain temporalily saved file.
+%
+%     'doverbose' 
+%                     Print out current estimating process.
+%                     
+%
+% OUTPUTS:
+%
+%     DCC_mat             P by P by T array of conditional correlations
+%
+%
+% EXAMPLES :
+%
+%     DCC_mat = DCC_CAPS2(roi_values, 'whiten', 'simple', 'doverbose');
+%     DCC_mat = DCC_CAPS2(roi_values, 'whiten', 'doverbose', 'dosaveload', savedir);
+%
+% Modified by J. J. Lee.
+% 2017.07.14
+
+
+%% Get optional variables
+
+dowhiten = 0;
+onlywhiten = 0;
+doresid = 0;
+dopar = 0;
+dosaveload = 0;
+dosimple = 0;
+doflat = 1;
+doverbose = 0;
+
+% optional inputs with default values
+for i = 1:length(varargin)
+    if ischar(varargin{i})
+        switch varargin{i}
+
+            case {'white', 'whiten'}
+                dowhiten = 1;
+                
+            case {'onlywhiten'}
+                dowhiten = 1;
+                onlywhiten = 1;
+                
+            case {'simple'}
+                dosimple = 1;
+                
+            case {'resid', 'residualize', 'X', 'model'}
+                doresid = 1; 
+                X = varargin{i + 1}; varargin{i + 1} = [];
+                
+            case {'parellel'}
+                dopar = 1;
+                
+            case {'dosaveload'}
+                dosaveload = 1;
+                savedir = varargin{i + 1}; varargin{i + 1} = [];
+                
+            case {'noflat'}
+                doflat = 0;
+
+            case {'doverbose'}
+                doverbose = 1;
+                
+            otherwise, warning(['Unknown input string option:' varargin{i}]);
+        end
+    end
+end
+
+
+%% Application of optional variables
+
+[t1,k1]=size(roi_values);
+
+if doresid
+   
+    % break up for efficiency - otherwise Matlab slows down with large
+    % matrices...or at least it used to...
+    px = pinv(X);
+    pxy = px * roi_values;
+    xpxy = X * pxy;
+    roi_values = roi_values - xpxy; % X * pinv(X) * y;
+    
+end
+
+if dowhiten
+    
+    Mdl = arima(1,0,1);
+    
+    opts = optimoptions('fmincon');
+    opts.Algorithm = 'sqp';
+    deltaX = 10^(-5);
+    opts.TolX = deltaX;
+    opts.TolCon = 10^(-6);
+    opts.MaxFunEvals = 1000;
+    
+    indx = 0;
+    fprintf('ARMA(1,1) on %d variables: %05d', k1, indx);
+    
+    if dopar
+        parfor k = 1:k1
+            EstMdl = estimate(Mdl, roi_values(:,k), 'Display', 'off', 'options', opts);
+            [res, v] = infer(EstMdl,roi_values(:,k));
+            roi_values(:,k) = res./sqrt(v);
+        end
+    else
+        for k = 1:k1
+            EstMdl = estimate(Mdl, roi_values(:,k), 'Display', 'off', 'options', opts);
+
+            [res, v] = infer(EstMdl,roi_values(:,k));
+            roi_values(:,k) = res./sqrt(v);
+
+            indx = indx + 1;
+            fprintf('\b\b\b\b\b%05d', indx);
+        end
+    end
+    
+    fprintf('\n');
+    
+end
+
+%% Main function
+
+if onlywhiten
+    
+    DCC_mat = roi_values;
+    
+elseif dosimple
+    
+    DCC_mat = DCC_CAPS2_garch(roi_values, doverbose);
+    if doflat
+        DCC_mat = shiftdim(DCC_mat, 2);
+        DCC_mat = DCC_mat(:, triu(true(k1,k1),1))';
+    end
+    
+elseif ~dosimple
+    
+    DCC_mat = zeros(k1*(k1-1)/2, t1);
+    dat = zeros(t1, 2);
+    tempdat = zeros(2, 2, t1);
+    
+    [~, ~, ~, ~, c_taketime] = sec2hms(0);
+    count_i = 0;
+    fprintf('total %.6d, estimated time %s : %.6d', k1*(k1-1)/2, c_taketime, count_i);
+    
+    
+    if dosaveload && exist(savedir, 'file')
+        load(savedir);
+        startpoint = find(DCC_mat==0, 1);
+    else
+        startpoint = 1;
+    end
+    
+    
+    
+    if dopar
+        parfor c_i = 1:k1*(k1-1)/2
+            c2_i = ceil(sqrt(2*c_i+0.25)-0.5); % n(n-1)/2 < x < n(n+1)/2.  sqrt(2x+1/4)-1/2 <= n < sqrt(2x+1/4)+1/2.
+            c1_i = c_i - c2_i*(c2_i-1)/2;
+            c2_i = c2_i + 1; % because of triu
+            dat = [roi_values(:,c1_i) roi_values(:,c2_i)];
+            tempdat = DCC_CAPS2_garch(dat, doverbose);
+            DCC_mat(c_i,:) = squeeze(tempdat(1,2,:));
+        end
+    else
+        tic;
+        for c2_i = 2:k1
+            for c1_i = 1:(c2_i-1)
+                
+                count_i = count_i + 1;
+                if count_i >= startpoint
+                    dat = [roi_values(:,c1_i) roi_values(:,c2_i)];
+                    tempdat = DCC_CAPS2_garch(dat, doverbose);
+                    DCC_mat(count_i,:) = squeeze(tempdat(1,2,:));
+                    
+                    if mod(count_i, 10) ~= 1
+                        fprintf(repmat('\b', 1, 6));
+                        fprintf('%.6d', count_i);
+                    elseif mod(count_i, 10) == 1
+                        c_time = toc;
+                        [~, ~, ~, ~, c_taketime] = sec2hms(c_time * (k1*(k1-1)/2) / count_i);
+                        fprintf(repmat('\b', 1, 32));
+                        fprintf('%s : %.6d', c_taketime, count_i);
+                    end
+                    
+                    if dosaveload && mod(count_i, 1800) == 0 % almost 1 hr
+                        save(savedir, 'out');
+                    end
+                end
+                
+            end
+        end
+    end
+    
+    if ~doflat
+        out_temp_3d = zeros(k1,k1,t1);
+        for t_i = 1:t1
+            out_temp_2d = zeros(k1,k1);
+            out_temp_2d(triu(true(k1,k1),1)) = DCC_mat(:,t_i);
+            out_temp_3d(:,:,t_i) = out_temp_2d + out_temp_2d';
+        end
+        DCC_mat = out_temp_3d;
+    end
+end
+
+fprintf('    done.\n');
+    
+
+
+end
---
+++ DCC/DCC_CAPS2_garch.m
@@ -0,0 +1,109 @@
+function Ct = DCC_CAPS2_garch(dat, doverbose)
+
+%% BASIC setting : Find zero timeseries to replace it with NaN
+
+[t2, k2_all] = size(dat);
+
+miscol = [];
+for i = 1:k2_all
+    miscol = [miscol isequal(dat(:,i), repmat(0,t2,1))];
+end
+miscol = logical(miscol);
+
+if any(miscol)
+    doNaN = 1;
+    dat(:,miscol) = [];
+    wh_val = ones(k2_all, k2_all);
+    wh_val(miscol,:) = 0;
+    wh_val(:,miscol) = 0;
+    wh_val = logical(wh_val);
+else
+    doNaN = 0;
+end
+
+%% BASIC setting : Parameter
+
+[t2, k2] = size(dat);
+
+dccP = 1;
+dccQ = 1;
+archP = repmat(1, 1, k2);
+garchQ = repmat(1, 1, k2);
+
+
+%% Main Function
+        
+% Now lest do the univariate garching using fattailed_garch as it's faster then garchpq
+
+stdresid = dat;
+options = optimset('fmincon');
+options = optimset(options, 'Display', 'off', 'Diagnostics', 'off', 'MaxFunEvals', 1000*max(archP+garchQ+1), ...
+    'MaxIter', 1000*max(archP+garchQ+1), 'LargeScale', 'off', 'MaxSQPIter', 1000);
+for i=1:k2
+    if doverbose, fprintf(1,'Estimating GARCH model for Series %d\n',i); end
+    [univariate{i}.parameters, ~, ~, ~, univariate{i}.ht, ~, dcc_exit] = ...
+        fattailed_garch_CAPS2(dat(:,i), archP(i), garchQ(i), 'NORMAL', [], options);
+    stdresid(:,i) = dat(:,i)./sqrt(univariate{i}.ht);
+    if dcc_exit < 1
+        error('DCC does not converge. : fattailed_garch_CAPS2');
+    end
+end
+
+
+dccstarting = [ones(1,dccP)*.01/dccP ones(1,dccQ)*.97/dccQ];
+if doverbose, fprintf(1,'\n\nEstimating the DCC model\n'); end
+
+[dccparameters,~,dcc_exit,~,~,~] = fmincon('dcc_mvgarch_likelihood', dccstarting, ones(size(dccstarting)), ...
+    [1-2*options.TolCon], [], [], zeros(size(dccstarting))+2*options.TolCon, [], [], ...
+    options, stdresid, dccP, dccQ);
+
+if dcc_exit < 1
+    error('DCC does not converge. : dcc_mvgarch_liklihood');
+end
+
+
+
+% We now have all of the estimated parameters
+parameters = [];
+H = zeros(t2,k2);
+for i= 1:k2
+    parameters = [parameters; univariate{i}.parameters];
+    H(:,i) = univariate{i}.ht;
+end
+parameters = [parameters; dccparameters'];
+
+
+
+%We now have Ht and the likelihood
+if ~doNaN
+    [~, Ct, ~, ~] = dcc_mvgarch_full_likelihood(parameters, dat, archP, garchQ, dccP, dccQ);
+elseif doNaN
+    [~, Ct_temp, ~, ~] = dcc_mvgarch_full_likelihood(parameters, dat, archP, garchQ, dccP, dccQ);
+    Ct = NaN(k2_all,k2_all,t2);
+    Ct(repmat(wh_val, 1, 1, t2)) = Ct_temp;
+end
+    
+% [~, Rt, ~, ~] = dcc_mvgarch_full_likelihood(parameters, dat, archP, garchQ, dccP, dccQ);
+% Ht = zeros(k2,k2,t2);
+% if ~doNaN
+%     Ct = zeros(k2,k2,t2);
+%     Hstd = H.^(0.5);
+%     
+%     for i= 1:t2
+%         Ht(:,:,i) = diag(Hstd(i,:)) * Rt(:,:,i) * diag(Hstd(i,:));
+%         Ct(:,:,i) = Ht(:,:,i) ./ sqrt(diag(Ht(:,:,i))*diag(Ht(:,:,i))');
+%     end
+%     
+% elseif doNaN
+%     Ct_temp = zeros(k2,k2,t2);
+%     Hstd = H.^(0.5);
+%     
+%     for i= 1:t2
+%         Ht(:,:,i) = diag(Hstd(i,:)) * Rt(:,:,i) * diag(Hstd(i,:));
+%         Ct_temp(:,:,i) = Ht(:,:,i) ./ sqrt(diag(Ht(:,:,i))*diag(Ht(:,:,i))');
+%     end
+%     
+%     Ct = NaN(k2_all,k2_all,t2);
+%     Ct(repmat(wh_val, 1, 1, t2)) = Ct_temp;
+% end
+
