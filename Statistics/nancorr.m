8d5d738d34a10ab857705ae9b6cd531dc702babf
admin <admin@Wissen.local>
2019-11-22 19:43:00 +0900

    updated nancorr.m

--- Statistics/nancorr.m
+++ Statistics/nancorr.m
@@ -2,25 +2,25 @@ function C = nancorr( X,Y )
 % NANCORR calculates the sample correlation coefficient
 %    for the series with NaNs expected.
 %    X is the one series, Y is another.
-X=X(:);
-Y=Y(:);
-L1=length(X);
-L2=length(Y);
+% X=X(:);
+% Y=Y(:);
+L1=size(X,1);
+L2=size(Y,1);
 
 if L1 ~= L2
     error('The samples must be of the same length')
 end
 
-for i=1:L1,
-    if isnan(X(i)),
-        Y(i)=nan;
+for i=1:L1
+    if any(isnan(X(i,:)))
+        Y(i,:)=NaN;
     end
-    if isnan(Y(i)),
-        X(i)=nan;
+    if any(isnan(Y(i,:)))
+        X(i,:)=NaN;
     end
 end
         
 Xm=nanmean(X);
 Ym=nanmean(Y);
-C=nansum((X-Xm).*(Y-Ym))/sqrt((nansum((X-Xm).^2))*(nansum((Y-Ym).^2)));
+C=nansum((X-Xm).*(Y-Ym))./sqrt((nansum((X-Xm).^2)).*(nansum((Y-Ym).^2)));
 end
