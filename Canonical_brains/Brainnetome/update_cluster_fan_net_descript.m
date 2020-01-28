adf14794c24d9c18aba0d99dea1b8e5f6d94c254
wanirepo <choongwan.woo@gmail.com>
2020-01-28 15:52:01 +0900

    code for updating cluster_fac_net descript

---
+++ Canonical_brains/Brainnetome/update_cluster_fan_net_descript.m
@@ -0,0 +1,37 @@
+
+cluster_Fan_Net.descrip2{2,1} = 'This is manual assignment of Buckner large-scale network';
+cluster_Fan_Net.descrip2{2,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Caudate', 'NAC', 'Putamen+GP', 'Amygdala', 'Hippocampus', 'Thalamus', 'PAG', 'Brainstem', 'cerebellum'}';
+
+cluster_Fan_Net.descrip2{3,1} = 'This is manual assignment of Buckner large-scale network';
+cluster_Fan_Net.descrip2{3,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Brainstem/Cerebellum'}';
+
+cluster_Fan_Net.descrip2{4,1} = 'Region-level assignment. See cluster_names';
+cluster_Fan_Net.descrip2{4,2} = cluster_Fan_Net.cluster_names;
+
+cluster_Fan_Net.descrip2{5,1} = 'Pain-related region assignment. See pain_cluster_names';
+cluster_Fan_Net.descrip2{5,2} = cluster_Fan_Net.pain_cluster_names;
+
+cluster_Fan_Net.descrip2{6,1} = 'Anatomical level assignment without functional network. See anat_cluster_names. Fr: Frontal, Tem: Temporal, Pr: Parietal, Ins: Insular, Lim: Limbic, Occ: Occipital, SbC: Subcortical, Cb: Cerebellar, BS: Brainstem';
+cluster_Fan_Net.descrip2{6,2} = cluster_Fan_Net.anat_cluster_names;
+
+cluster_Fan_Net.descrip2{7,1} = 'Laterality. -1: Left, 1: Right, 0: No laterality';
+
+cluster_Fan_Net.descrip2{8,1} = 'This is different from {3}, provided by Brainnetome webpage';
+cluster_Fan_Net.descrip2{8,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Brainstem/Cerebellum'}';
+
+cluster_Fan_Net.descrip2{9,1} = 'This is same with {3}, but separated cerebellum and brainstem';
+cluster_Fan_Net.descrip2{9,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Cerebellum', 'Brainstem'}';
+
+cluster_Fan_Net.descrip2{10,1} = 'This is different from {3}, provided by Brainnetome webpage';
+cluster_Fan_Net.descrip2{10,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Cerebellum', 'Brainstem'}';
+
+cluster_Fan_Net.descrip2{11,1} = 'this is an update from {2}, changed two regions'' network assignment';
+cluster_Fan_Net.descrip2{11,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Caudate', 'NAC', 'Putamen+GP', 'Amygdala', 'Hippocampus', 'Thalamus', 'PAG', 'Brainstem', 'cerebellum'}';
+
+cluster_Fan_Net.descrip2{12,1} = 'this is an update from {3}, changed two regions'' network assignment';
+cluster_Fan_Net.descrip2{12,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Brainstem/Cerebellum'}';
+
+cluster_Fan_Net.descrip2{13,1} = 'this is an update from {9}, changed two regions'' network assignment';
+cluster_Fan_Net.descrip2{13,2} = {'visual', 'somatomotor', 'dAttention', 'vAttention', 'Limbic', 'Frontoparietal', 'Default', 'Subcortical', 'Cerebellum', 'Brainstem'}';
+
+    
