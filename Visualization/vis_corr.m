3ce6015732da369ebce299232ea554a4281d2de7
jaejoonglee92 <jaejoonglee92@gmail.com>
2021-06-07 15:34:10 +0900

    update documentation for vis_corr

--- Visualization/vis_corr.m
+++ Visualization/vis_corr.m
@@ -1,69 +1,98 @@
 function out = vis_corr(r, varargin)
 
-% Draw a nice correlation figure
+% This function draws a correlation matrix.
 %
-% Usage:
-% -------------------------------------------------------------------------
-% function h = vis_corr(r, varargin)
 %
-% Inputs:
-% -------------------------------------------------------------------------
-% r     square matrix (doesn't need to be a correlation)
+% :Usage:
+% ::
+%     out = vis_corr(r, varargin)
 %
-% Optional inputs: Enter keyword followed by variable with values
-% 'colormap'      colormap (default uses a colormap from colorbrewer2)
-% 'clim'          two numbers [CLOW CHIGH] for color scaling of imagesc 
-%                 (see >> help imagesc)
-% 'colorbar'      show colorbar
 %
-% Outputs:
-% -------------------------------------------------------------------------
-% h              graphic handles 
+% :Input:
+% ::
+%   - r                  Adjacency matrix (doesn't need to be a correlation)
 %
-% Examples: 
-% -------------------------------------------------------------------------
+%
+% :Optional Input:
+%
+%   - group              Group assignment of nodes. ex) [1,1,1,2,2,2,3,4]
+%   - group_linecolor    Color of lines separating groups
+%   - group_linewidth    Width of lines separating groups
+%   - group_mean         If specified, connections are averaged for each
+%                        group of nodes (indicated in 'group').
+%   - group_sum          If specified, connections are summed for each
+%                        group of nodes (indicated in 'group').
+%   - group_color        Colors indicating each group
+%   - group_tick         If specified, ticks for separating groups are
+%                        added on x and y axis.
+%   - group_tickstyle    Style for group ticks.
+%                        'edge': Ticks are located in every edge
+%                                between adjacent nodes. (default)
+%                        'center': Ticks are located in center of each
+%                                  node.
+%   - group_tickwidth    Width of group ticks.
+%   - group_ticklength   Length of group ticks.
+%   - group_tickoffset   Offset of group ticks.
+%   - smooth             Apply 2D smoothing (sigma = 1) for better display.
+%   - sig                Mark specific connections. Followed by a matrix
+%                        specifying significant connections.
+%   - sig_linewidth      Width of lines for significant connections.
+%   - sig_color          Color of lines for significant connections.
+%   - noplot             Do not generate figures. Useful for obtaining
+%     or nodisplay       group-averaged correlation values.
+%   - same_fig           Drawing onto the current active figure (gcf).
+%                        If not specified, a new figure will be generated.
+%   - colormap           Color scheme of this figure.
+%     or colors
+%   - clim               Colormap limit of this figure.
+%                        Followed by [CLOW CHIGH].
+%   - colorbar           Show colorbar.
+%   - nolines            No grid and border lines of correlation matrix.
+%   - nolines_1st        No fine-scaled grid lines.
+%   - nolines_2nd        No coarse-scaled grid lines.
+%   - nolines_out        No border lines.
+%   - nolines_out        No border lines.
+%   - line_color         Color of grid and border lines.
+%   - line_width         Width of grid and border lines.
+%   - triangle           Leave only lower triangle by cropping upper one.
+%   - no_triangle_line   No lines for triangle.
+%   - triangle_color     Color of triangle lines.
+%   - triangle_width     Width of triangle lines.
+%
+%
+% :Output:
+% ::
+%   - out                Handle of the generated figure
+%
+%
+% :Example:
+% ::
 % % data
-% r = rand(30,30);
+% r = corr(rand(30,30));
 % h = vis_corr(r, 'clim', [0 1])
 %
 % savename = 'example_corr.png';
-% 
-% try
-%     pagesetup(gcf);
-%     saveas(gcf, savename);
-% catch
-%     pagesetup(gcf);
-%     saveas(gcf, savename);    
-% end
+% pagesetup(gcf);
+% saveas(gcf, savename);    
 %
-% -------------------------------------------------------------------------
-% Copyright (C) 2015  Wani Woo
-
-% Programmers' notes:
-
-col_map = [0.3686    0.3098    0.6353
-    0.1961    0.5333    0.7412
-    0.4000    0.7608    0.6471
-    0.6706    0.8667    0.6431
-    0.9020    0.9608    0.5961
-    1.0000    1.0000    1.0000
-    0.9961    0.8784    0.5451
-    0.9922    0.6824    0.3804
-    0.9569    0.4275    0.2627
-    0.8353    0.2431    0.3098
-    0.6196    0.0039    0.2588];
-
-col_map = interp1(1:size(col_map,1), col_map, linspace(1, size(col_map,1), 64));
-
-do_display = 1;
-docolorbar = 0;
-dolines_1st = 1;
-dolines_2nd = 1;
-dolines_out = 1;
-do_triangle = 0;
-
-lcolor = [.7 .7 .7; .3 .3 .3; 0 0 0];
-lwidth = [0.5 1 1.5]; % one five outline
+%
+%     Author and copyright information:
+%
+%     Copyright (C) May 2015  Choong-Wan Woo & Jae-Joong Lee
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ..
 
 dogroupsort = 0;
 dogroupline = 0;
@@ -91,28 +120,36 @@ tickoffset = 1;
 same_fig = 0;
 no_triangle_line = 0;
 
+col_map = [0.3686    0.3098    0.6353
+    0.1961    0.5333    0.7412
+    0.4000    0.7608    0.6471
+    0.6706    0.8667    0.6431
+    0.9020    0.9608    0.5961
+    1.0000    1.0000    1.0000
+    0.9961    0.8784    0.5451
+    0.9922    0.6824    0.3804
+    0.9569    0.4275    0.2627
+    0.8353    0.2431    0.3098
+    0.6196    0.0039    0.2588];
+
+col_map = interp1(1:size(col_map,1), col_map, linspace(1, size(col_map,1), 64));
+
+clim = [];
+
+do_display = 1;
+docolorbar = 0;
+dolines_1st = 1;
+dolines_2nd = 1;
+dolines_out = 1;
+do_triangle = 0;
+
+lcolor = [.7 .7 .7; .3 .3 .3; 0 0 0];
+lwidth = [0.5 1 1.5]; % one five outline
+
 for i = 1:length(varargin)
     if ischar(varargin{i})
         switch varargin{i}
             % functional commands
-            case {'colormap', 'colors'}
-                col_map = varargin{i+1};
-            case {'colorbar'}
-                docolorbar = 1;
-            case {'nolines'}
-                dolines_1st = 0;
-                dolines_2nd = 0;
-                dolines_out = 0;
-            case {'nolines_1st'}
-                dolines_1st = 0;
-            case {'nolines_2nd'}
-                dolines_2nd = 0;
-            case {'nolines_out'}
-                dolines_out = 0;
-            case {'line_color'}
-                lcolor = varargin{i+1};
-            case {'line_width'}
-                lwidth = varargin{i+1};
             case {'group'}
                 dogroupsort = 1;
                 dogroupline = 1;
@@ -147,12 +184,6 @@ for i = 1:length(varargin)
                 tickoffset = varargin{i+1};
             case {'smooth'}
                 dosmooth = 1; % 2d gaussian smoothing (imgaussfilt) with sigma = 1
-            case {'triangle'}
-                do_triangle = 1;
-            case {'triangle_color'}
-                triangle_col = varargin{i+1};
-            case {'triangle_width'}
-                triangle_width = varargin{i+1};
             case {'sig'}
                 do_sig = true;
                 sig_matrix = varargin{i+1};
@@ -164,8 +195,35 @@ for i = 1:length(varargin)
                 do_display = 0;
             case {'same_fig'}
                 same_fig = 1;
+            case {'colormap', 'colors'}
+                col_map = varargin{i+1};
+            case {'clim'}
+                clim = varargin{i+1};
+                clim_descript = 'optional input';
+            case {'colorbar'}
+                docolorbar = 1;
+            case {'nolines'}
+                dolines_1st = 0;
+                dolines_2nd = 0;
+                dolines_out = 0;
+            case {'nolines_1st'}
+                dolines_1st = 0;
+            case {'nolines_2nd'}
+                dolines_2nd = 0;
+            case {'nolines_out'}
+                dolines_out = 0;
+            case {'line_color'}
+                lcolor = varargin{i+1};
+            case {'line_width'}
+                lwidth = varargin{i+1};
+            case {'triangle'}
+                do_triangle = 1;
             case {'no_triangle_line'}
                 no_triangle_line = 1;
+            case {'triangle_color'}
+                triangle_col = varargin{i+1};
+            case {'triangle_width'}
+                triangle_width = varargin{i+1};
         end
     end
 end
@@ -224,18 +282,9 @@ if do_display
         tr(r==0) = [];
         m = max(prctile(tr(:), 97.5), abs(prctile(tr(:), 2.5)));
     end
-    clim = [-m m];
-    clim_descript = 'default: upper 97.5%, lower 2.5%';
-    
-    for i = 1:length(varargin)
-        if ischar(varargin{i})
-            switch varargin{i}
-                % functional commands
-                case {'clim'}
-                    clim = varargin{i+1};
-                    clim_descript = 'optional input';
-            end
-        end
+    if isempty(clim)
+        clim = [-m m];
+        clim_descript = 'default: upper 97.5%, lower 2.5%';
     end
     
     % check if r is a square matrix
@@ -387,7 +436,8 @@ if do_display
     end
     
     if do_triangle
-        patch([-1, size(r,1)+2, size(r,1)+2], [-1, -1, size(r,1)+2], 'w', 'edgecolor', 'w');
+        ws = 3;
+        patch([-ws, size(r,1)+1+ws, size(r,1)+1+ws], [-ws, -ws, size(r,1)+1+ws], 'w', 'edgecolor', 'w');
         if ~no_triangle_line
             patch([.5, .5, size(r,1)+.5], [.5, size(r,1)+.5, size(r,1)+.5], 'w', 'edgecolor', triangle_col, 'facealpha', 0, 'LineWidth', triangle_width);
         end
