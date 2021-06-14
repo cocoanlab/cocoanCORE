447f8b030ee82ac3d48e274d2a52c6686982fb14
jaejoonglee92 <jaejoonglee92@gmail.com>
2021-06-14 18:56:10 +0900

    bugfix ffcorr: trim over 1 under -1

--- Misc_utilities/ffcorr.m
+++ Misc_utilities/ffcorr.m
@@ -1,8 +1,10 @@
 function y = ffcorr(x)
-% Fast, Flattend correlation
+% Fast & Flattened correlation
                 
 [n,p] = size(x);
 zx = (x - mean(x)) ./ (std(x) * (n-1).^0.5); % z-scoring divided by (n-1).^0.5; do not use 'zscore.m' that ignores zero array... sad.
 y = subsref(zx.' * zx, struct('type', {'()'}, 'subs', {{(1:p) > (1:p).'}}));
+wh_bad = abs(y)>1;
+y(wh_bad) = sign(y(wh_bad));
 
 end
